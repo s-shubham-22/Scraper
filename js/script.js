65688d433797130a953f8543437f137f4eb340c1
Shubham Sareliya <sareliashubham9999@gmail.com>
2022-03-03 00:07:05 +0530

    worked on 'Let Scrap' Page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+// To load header and footer
 $("#header").load("header.html");
 $("#footer").load("footer.html");
+
+// Scroll to top of the page
 $('html, body').animate({
     scrollTop: 0
 }, 'fast');
+
+// insert 'active' class in in proper navbar item
+window.addEventListener('load', function() {
+    path = document.location.pathname;
+    page = path.split("/").pop();
+    filename = page.split(".").shift();
+    $("#" + filename).addClass("active");
+})
