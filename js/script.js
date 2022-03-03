172027bbea70f45b4e644bc562ceef69c3f35197
Shubham Sareliya <sareliashubham9999@gmail.com>
2022-03-04 00:03:40 +0530

    Worked on Letsscrap Page

--- js/script.js
+++ js/script.js
@@ -14,3 +14,23 @@ window.addEventListener('load', function() {
     filename = page.split(".").shift();
     $("#" + filename).addClass("active");
 })
+
+const btn = $('#form1-btn');
+const sb = $('#choice1');
+const num = $('#numOfEle');
+
+var choice = 0;
+btn.onclick = (event) => {
+    event.preventDefault();
+    // show the selected index
+    choice = sb.selectedIndex;
+    if (choice === 1) {
+        // for (let i = 1; i <= num; i++) {
+        //     console.log('Hello');
+        //     // $('#formId').append(`<input class="form-control" type="text" placeholder="Label for Column" aria-label="default input example" id="label${i}">
+        //     // <input class="form-control" type="text" placeholder="Selector" aria-label="default input example" id="selector${i}">`)
+        // }
+    } else {
+        alert('You have choosen web scraping using XPath');
+    }
+};
