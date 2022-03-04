b3b75a9b46f69c47c4cdd97331a86b73c8f8bf73
Shubham Sareliya <sareliashubham9999@gmail.com>
2022-03-04 12:10:56 +0530

    Updated

--- testing/caller.js
+++ testing/caller.js
@@ -2,7 +2,8 @@ const spawn = require('child_process').spawn;
 
 // Initialise the data
 const data = {
-array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    url: 'https://www.imdb.com/chart/top/',
+    array: ['#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.sg-row > div.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span:nth-child(4) > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(9) > div > div > div > div > div > div > div > div.sg-col.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20.s-list-col-right > div > div > div.a-section.a-spacing-none.s-padding-right-small.s-title-instructions-style > h2 > a > span#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.sg-row > div.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span:nth-child(4) > div.s-main-slot.s-result-list.s-search-results.sg-row > div > div > div > div > div > div > div > div > div.sg-col.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20.s-list-col-right > div > div > div.a-section.a-spacing-none.s-padding-right-small.s-title-instructions-style > h2 > a > span']
 }
 
 // We need to stringify the data as
